c40c1dfdab3b0f03070c343a535b6e0a3a98bfa1
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2023-11-13 10:10:28 +0100

    Most user getters and setters seem unnecessary...

--- src/main/java/nl/itvitae/jdbcauthenticationmanagerdemotwo/User.java
+++ src/main/java/nl/itvitae/jdbcauthenticationmanagerdemotwo/User.java
@@ -29,24 +29,4 @@ public class User {
     public String getUsername() {
         return username;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
